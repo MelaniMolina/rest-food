1ef040ff749dc6c84eb9615a7363abf2981e0ca3
MelaniMolina <melani.molina@epn.edu.ec>
2024-01-05 10:01:15 -0500

    hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  ApiKeyGoogleMaps:'AIzaSyAqso1wFkYLCMnwFiHu0_vNbAYSWLKyhII',
   firebaseConfig: {
     apiKey: "AIzaSyBlSX6swTCQ-4G_3ca3Jy7WKwXHiY6ScuQ",
     authDomain: "huecascomida.firebaseapp.com",
@@ -9,4 +10,5 @@ export const environment = {
     appId: "1:471323262469:web:08f49b071131c60765266c",
     measurementId: "G-9FYRFP31M9"
   }
+  
 };
