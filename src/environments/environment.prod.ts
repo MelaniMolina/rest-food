0a72624f24477112f1987cdde743096552822de3
MelaniMolina <melani.molina@epn.edu.ec>
2024-01-05 09:15:24 -0500

    finish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlSX6swTCQ-4G_3ca3Jy7WKwXHiY6ScuQ",
+    authDomain: "huecascomida.firebaseapp.com",
+    projectId: "huecascomida",
+    storageBucket: "huecascomida.appspot.com",
+    messagingSenderId: "471323262469",
+    appId: "1:471323262469:web:08f49b071131c60765266c",
+    measurementId: "G-9FYRFP31M9"
+  }
 };
